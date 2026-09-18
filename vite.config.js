96b790358b40c941f5151dbecbda6a2dc3c3daf4
Bayu Tri Prayitno <130425189+bayu3prayitno@users.noreply.github.com>
2026-09-18 16:15:05 +0700

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portofolio.github.io/',
+  base: '/',
+  // base: '/portofolio.github.io/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
